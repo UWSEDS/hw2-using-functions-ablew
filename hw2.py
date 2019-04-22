d7f4b4b76f3fbabb7b0b837a77c9b0e4d0d3c9f9
Agustin Lew <ablew@uw.edu>
2019-04-22 10:52:56 -0700

    hw2 upload

---
+++ hw2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv("https://data.ny.gov/api/views/5xaw-6ayf/rows.csv?accessType=DOWNLOAD")
+
+def test_create_dataframe(df):
+    checks = []
+    
+    if list(df) == ['Draw Date', 'Winning Numbers', 'Mega Ball', 'Multiplier']:
+        checks.append(1)
+    else:
+        checks.append(0)
+    
+    types = []
+    
+    for i in range(len(list(df))):
+        types.append(str(df[list(df)[i]].dtypes))
+        
+    if types == ['object', 'object', 'int64', 'float64']:
+        checks.append(1)
+    else:
+        checks.append(0)
+        
+    if len(df) >= 10:
+        checks.append(1)
+    else:
+        checks.append(0)
+        
+    if sum(checks) == 3:
+        return True
+    else:
+        return False
+    
